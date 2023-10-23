54979a7ea374c601ecd5f0f17a712b1c4e27f942
Nikola Milanović <nikola.milanovic.dev@gmail.com>
2023-10-23 10:13:04 +0200

    Lottery project finished

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 import {LinkToken} from "../test/mocks/LinkToken.sol";
+
+pragma solidity ^0.8.18;
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -36,7 +37,7 @@ contract HelperConfig is Script {
                 interval: 30,
                 vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
-                subscriptionId: 0,
+                subscriptionId: 6281,
                 callbackGasLimit: 500000, // 500,000 gas
                 link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
                 deployerKey: vm.envUint("PRIVATE_KEY")
@@ -66,7 +67,7 @@ contract HelperConfig is Script {
                 interval: 30,
                 vrfCoordinator: address(vrfCoordinatorMock),
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
-                subscriptionId: 0,
+                subscriptionId: 0, // our script will add this
                 callbackGasLimit: 500000, // 500,000 gas
                 link: address(link),
                 deployerKey: DEFAULT_ANVIL_KEY
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: MIT
+
+// We need this file to run DeployRuffle.s.sol because it expects a subscription id
+
 pragma solidity ^0.8.18;
 
 import {Script, console} from "forge-std/Script.sol";
@@ -23,8 +26,6 @@ contract CreateSubscription is Script {
         vm.startBroadcast(deployerKey);
         uint64 subId = VRFCoordinatorV2Mock(vrfCoordinator)
             .createSubscription();
-        console.log("Your sub Id is: ", subId);
-        console.log("Please update subscriptionId in HelperConfig.s.sol");
         vm.stopBroadcast();
         return subId;
     }
@@ -59,7 +60,7 @@ contract FundSubscription is Script {
         uint256 deployerKey
     ) public {
         console.log("Funding subscription: ", subId);
-        console.log("Using vrfCoordinator : ", vrfCoordinator);
+        console.log("Using vrfCoordinator: ", vrfCoordinator);
         console.log("On ChainID: ", block.chainid);
         if (block.chainid == 31337) {
             vm.startBroadcast(deployerKey);
